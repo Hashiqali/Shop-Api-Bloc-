16969e11350c425ac01f4eed1f047be80888ef1f
ashi <hashiqali3080@gmail.com>
2024-02-12 09:14:00 +0530

    first

---
+++ lib/Favourite/Favourite.dart
@@ -0,0 +1,120 @@
+// ignore_for_file: collection_methods_unrelated_type
+
+import 'package:bloc_project/Favourite/bloc/favourite_bloc.dart';
+import 'package:bloc_project/model/model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+List<ProductModel> favourites = [];
+
+class FavouriteScreen extends StatefulWidget {
+  const FavouriteScreen({super.key});
+
+  @override
+  State<FavouriteScreen> createState() => _FavouriteScreenState();
+}
+
+class _FavouriteScreenState extends State<FavouriteScreen> {
+  final favBloc = FavouriteBloc();
+  @override
+  void initState() {
+    favBloc.add(FavInitializeEvent());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<FavouriteBloc, FavouriteState>(
+      bloc: favBloc,
+      listenWhen: (previous, current) => current is FavActionState,
+      buildWhen: (previous, current) => current is! FavActionState,
+      listener: (context, state) {},
+      builder: (context, state) {
+        switch (state.runtimeType) {
+          case FavSuccessState:
+            final value = state as FavSuccessState;
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: const Color.fromARGB(255, 45, 44, 44),
+                centerTitle: true,
+                title: const Text(
+                  'Favourites',
+                  style: TextStyle(
+                      fontFamily: 'HASHI', fontWeight: FontWeight.w900),
+                ),
+              ),
+              body: ListView.builder(
+                  itemCount: favourites.length,
+                  itemBuilder: (ctx, index) {
+                    final data = value.products[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Container(
+                        height: 250,
+                        decoration: BoxDecoration(
+                            color: Colors.amber,
+                            image: DecorationImage(
+                                image: NetworkImage(data.imageurl),
+                                fit: BoxFit.fill),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10)),
+                            width: double.infinity,
+                            height: 50,
+                            child: Row(children: [
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 20),
+                                  child: Text(
+                                    data.name,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.w900,
+                                      fontFamily: 'HASHI',
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  IconButton(
+                                      onPressed: () {
+                                        favBloc
+                                            .add(FavdeleteEvent(product: data));
+                                      },
+                                      icon: const Icon(
+                                        Icons.favorite,
+                                        color: Colors.black,
+                                      )),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(
+                                        Icons.shopping_bag_outlined,
+                                        color: Colors.black,
+                                      ))
+                                ],
+                              )
+                            ]),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            );
+        }
+        return Scaffold(
+          appBar: AppBar(),
+          body: const Center(
+            child: Text('Error'),
+          ),
+        );
+      },
+    );
+  }
+}
